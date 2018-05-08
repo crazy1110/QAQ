03b2e98cced376fa2dcd18c8ffeb9088e4155f2c
rivercome <897971480@qq.com>
2018-05-08 16:39:20 +0800

    完成6个表单类型

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import { Form, InputNumber, Input, DatePicker, Button, Select } from 'antd'
+import { Form, InputNumber, Input, DatePicker, Button, Select, Radio } from 'antd'
 import moment from 'moment'
 // 推荐在入口文件全局设置 locale
 import 'antd/dist/antd.css'
 import 'moment/locale/zh-cn'
+
 moment.locale('zh-cn')
 
 const FormItem = Form.Item
 const Option = Select.Option
 const TextArea = Input.TextArea
-
+const RadioButton = Radio.Button
+const RadioGroup = Radio.Group
 // 后台返回的数据格式
 const data = [
   {
@@ -19,40 +21,53 @@ const data = [
     'required': true,
     'type': 'input',
     // 'value': 100
-  },{
+  }, {
     'field': 'date',
     'text': '日期',
     'errorMessage': '请输入日期',
     'required': false,
     'type': 'date',
     // 'value': '2017-10-20'
-  },{
+  }, {
     'field': 'stuid',
     'text': '学号',
     'errorMessage': '请输入学号',
     'required': true,
     'type': 'char',
     // 'value': 'hello world'
-  },{
+  }, {
     'field': 'department',
     'text': '学院',
     'errorMessage': '请输入学院',
     'required': true,
     'type': 'select',
-    'value': '计算机与通信工程学院',
+    // 'value': '计算机与通信工程学院',
     'options': ['计算机科学与技术', '物联网工程', '电子信息工程', '通信工程', '生物医学工程']
+  }, {
+    'field': 'sex',
+    'text': '性别',
+    'errorMessage': '请选择性别',
+    'required': true,
+    'type': 'radio',
+    'options': ['nv', 'nan']
+  }, {
+    'field': 'textArea',
+    'text': '介绍',
+    'errorMessage': '请输入介绍',
+    'required': true,
+    'type': 'textArea'
   }
 ]
 
 // formItem css 样式
 const formItemLayout = {
   labelCol: {
-    xs: { span: 24 },
-    sm: { span: 6 },
+    xs: {span: 24},
+    sm: {span: 6},
   },
   wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 14 },
+    xs: {span: 24},
+    sm: {span: 14},
   }
 }
 
@@ -70,7 +85,7 @@ const tailFormItemLayout = {
 }
 
 const formLayout = {
-  width: '90%',
+  width: '80%',
   marginTop: 100,
   marginLeft: 'auto',
   marginRight: 'auto'
@@ -78,30 +93,32 @@ const formLayout = {
 
 @Form.create()
 export default class Home extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
     this.state = {
-      data:[]
+      data: []
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.getForm = this.getForm.bind(this)
   }
-  componentWillMount(){
+
+  componentWillMount () {
     console.log(this.props)
   }
-  getForm(){
-    fetch('url').then((res) =>{
+
+  getForm () {
+    fetch('url').then((res) => {
       const data = res
     })
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
+  handleSubmit (e) {
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values)
       }
-    });
+    })
   }
 
   /**
@@ -109,42 +126,57 @@ export default class Home extends Component {
    * @param item  json
    * @param Component
    */
-  switchItem(item) {
-    const type = item.type;
+  switchItem (item) {
+    const type = item.type
     switch (type) {
-      case 'int':
-        return <InputNumber style={{ width: '100%' }} />;
-        break;
+      case 'input':
+        return <InputNumber style={{width: '100%'}} />
+        break
       case 'char':
-        return <Input />;
-        break;
+        return <Input />
+        break
       case 'date':
-        return <DatePicker style={{ width: '100%' }} />;
-        break;
+        return <DatePicker style={{width: '100%'}} />
+        break
       case 'select':
         return (
           <Select>
-            {/*{*/}
-              {/*item.options.map((option, index) => {*/}
-                {/*return (<Option key={index} value={option}>{option}</Option>)*/}
-              {/*})*/}
-            {/*}*/}
+            {
+              item.options.map((option, index) => {
+                return (<Option key={index} value={option}>{option}</Option>)
+              })
+            }
           </Select>
         )
+        break
+      case 'radio':
+        return (
+          <RadioGroup>
+            {
+              item.options.map((option, index) => {
+                return (<Radio key={index} value={option}>{option}</Radio>)
+              })
+            }
+          </RadioGroup>
+        )
+        break
+      case 'textArea':
+        return <TextArea />
+        break
       default:
-        return <Input />;
-        break;
+        return <Input />
+        break
     }
   }
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
+  render () {
+    const {getFieldDecorator} = this.props.form
     return (
       <Form onSubmit={this.handleSubmit} style={formLayout}>
         {
           data.map((item, index) => {
             // type 为 date 日期格式需要强制转化为 moment 格式
-            item.value = item.type == 'date' ? moment(item.value, 'YYYY-MM-DD') : item.value;
+            item.value = item.type == 'date' ? moment(item.value, 'YYYY-MM-DD') : item.value
             return (
               <FormItem
                 key={index}
@@ -167,7 +199,7 @@ export default class Home extends Component {
         }
         <FormItem {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">
-            保存
+            提交
           </Button>
         </FormItem>
       </Form>
