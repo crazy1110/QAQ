013b91249d4da2599eee11ed2df7be8410fb1d06
Snowcying <347025712@qq.com>
2018-05-13 20:56:55 +0800

    增加radio和input功能 commit by cxy

--- src/components/RadioForm/index.js
+++ src/components/RadioForm/index.js
@@ -1,18 +1,16 @@
-import { Form, Input, Icon, Button, Radio, Modal } from 'antd'
-import { Component } from 'react'
+import {Form, Icon, Button, Radio, Modal} from 'antd'
+import {Component} from 'react'
 import RadioModel from './radioModel/index'
 import './index.css'
 
 const FormItem = Form.Item
-const RadioButton = Radio.Button
+// const RadioButton = Radio.Button
 const RadioGroup = Radio.Group
 
-let ID = 0
-let radioValueList = [], radioTitleList = []
+let ID = 0, radioValueList = [], radioTitleList = [],radioRequireList=[]
 
 class DynamicFieldSet extends Component {
-
-  constructor () {
+  constructor() {
     super()
     this.state = {
       radioVisible: false,
@@ -49,14 +47,26 @@ class DynamicFieldSet extends Component {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
+        const keys=this.props.form.getFieldValue('radioKeys')
+        let name=[],values=[],require=[]
+        for (let i=0;i<keys.length;i++) {
 
-        let obj = {}
-        obj.require = '1'
-        obj.type = 'radio'
-        obj.name = radioTitleList
-        obj.valuesList = radioValueList
+          if (keys[i] !== -1){
+            name.push(radioTitleList[i])
+            values.push(radioValueList[i])
+            require.push(radioRequireList[i])
+          }
+        }
 
-        // this.props.handleAddRadio(obj)
+        let obj={}
+        obj.require=require
+        obj.type='radio'
+        obj.name=name
+        obj.values=values
+        // obj.name=inputTitle
+        // obj.rules=inputRules
+        console.log('radioObj',obj)
+        this.props.handleAddRadio(obj)
 
         console.log('!!obj!!:', obj)
         console.log('value: ', values)
@@ -66,13 +76,13 @@ class DynamicFieldSet extends Component {
 
   }
 
-  handleSubmitRadio = (values, title) => {
+  handleSubmitRadio = (values, title,require) => {
 
     const {form} = this.props
 
     radioValueList.push(values)
     radioTitleList.push(title)
-
+    radioRequireList.push(require)
     this.setState({
       radioVisible: false,
     })
@@ -87,24 +97,24 @@ class DynamicFieldSet extends Component {
     console.log('createForm: ', values)
   }
 
-  render () {
+  render() {
     const {radioVisible} = this.state
     const {getFieldDecorator, getFieldValue} = this.props.form
     const formItemLayout = {
       labelCol: {
         xs: {span: 24},
-        sm: {span: 4},
+        sm: {span: 4}
       },
       wrapperCol: {
         xs: {span: 24},
-        sm: {span: 20},
+        sm: {span: 20}
       },
     }
     const formItemLayoutWithOutLabel = {
       wrapperCol: {
         xs: {span: 24, offset: 0},
-        sm: {span: 20, offset: 4},
-      },
+        sm: {span: 20, offset: 4}
+      }
     }
     getFieldDecorator('radioKeys', {initialValue: []})
     const radioKeys = getFieldValue('radioKeys')
@@ -116,6 +126,7 @@ class DynamicFieldSet extends Component {
       let len = currentRadioValues.length
       let options = []
       let title = radioTitleList[index]
+      let bool= radioRequireList[index] === 1 ? true : false
       for (let i = 0; i < len; i++) {
         if (radioValueList[index][i]) {
           options.push({label: radioValueList[index][i], value: radioValueList[index][i]})
@@ -126,24 +137,24 @@ class DynamicFieldSet extends Component {
           <FormItem
             {...formItemLayout}
             label={title}
-            required={false}
+            required={bool}
             key={k}
           >
             {getFieldDecorator(radioType)(
               <div>
-                <RadioGroup options={options} />
+                <RadioGroup options={options}/>
               </div>
             )}
 
             {radioKeys.length > 0 ? (
               <Icon
-                className="dynamic-delete-button"
-                type="minus-circle-o"
+                className='dynamic-delete-button'
+                type='minus-circle-o'
                 disabled={radioKeys.length === 0}
                 onClick={() => this.remove(k, 'radioKeys')}
               />
             ) : null}
-            <br />
+            <br/>
           </FormItem>
         )
       }
@@ -157,25 +168,25 @@ class DynamicFieldSet extends Component {
           {radioFormItems}
           <FormItem {...formItemLayoutWithOutLabel}>
 
-            <Button type="dashed" onClick={this.addRadio} style={{width: '60%'}}>
-              <Icon type="plus" /> Add radio
+            <Button type='dashed' onClick={this.addRadio} style={{width: '60%'}}>
+              <Icon type='plus'/> Add radio
             </Button>
 
 
           </FormItem>
           <FormItem {...formItemLayoutWithOutLabel}>
-            <Button type="primary" htmlType="submit">确认</Button>
+            <Button type='primary' htmlType='submit'>确认</Button>
           </FormItem>
 
 
         </Form>
         <div>
-          <Modal title="Title"
-                 visible={radioVisible}
-                 onCancel={() => {this.setState({radioVisible: false})}}
+          <Modal title='Title' visible={radioVisible} onCancel={() => {
+            this.setState({radioVisible: false})
+          }}
             // confirmLoading={confirmLoading}
           >
-            <RadioModel handleSubmitRadio={this.handleSubmitRadio} />
+            <RadioModel handleSubmitRadio={this.handleSubmitRadio} formType={'radio'}/>
 
           </Modal>
         </div>
